d1c5409604fe191b43001cd3efee10925bb9f392
Chris Tacey-Green <chris@taceygreen.com>
2019-01-09 13:07:57 +0000

    Add project files.

---
+++ OddPairing/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+// you can also use other imports, for example:
+// using System.Collections.Generic;
+
+// you can write to stdout for debugging purposes, e.g.
+// Console.WriteLine("this is a debug message");
+
+class Solution
+{
+    public int solution(int[] A)
+    {
+        // write your code in C# 6.0 with .NET 4.5 (Mono)
+        Dictionary<int, int> freqCounts = new Dictionary<int, int>();
+        foreach(var item in A)
+        {
+            var currentValue = 0;
+            freqCounts.TryGetValue(item, out currentValue);
+            freqCounts[item] = currentValue + 1;
+        }
+        foreach(var keyPair in freqCounts)
+        {
+            if(keyPair.Value % 2 != 0)
+            {
+                return keyPair.Key;
+            }
+        }
+        return 0;
+    }
+}
